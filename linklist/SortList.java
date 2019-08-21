9463b2bb2b4438ba4dc6a8fdd5c68432d877de0c
z944733142 <944733142@qq.com>
2019-08-21 20:27:14 +0800

    链表归并排序

--- linklist/SortList.java
+++ linklist/SortList.java
@@ -3,14 +3,30 @@ package algorithms.linklist;
 import java.util.List;
 
 /**
+ * 在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序。
+ *
+ * 示例 1:
+ *
+ * 输入: 4->2->1->3
+ * 输出: 1->2->3->4
+ *
+ * 归并排序
  * @author: shuo
  * @date: 2019/08/14
  */
 public class SortList {
+    public static void main(String[] args) {
+        ListNode l = new ListNode(4);
+        l.next = new ListNode(2);
+        l.next.next = new ListNode(1);
+        l.next.next.next = new ListNode(3);
+        SortList s = new SortList();
+        s.sortList(l);
+    }
     public ListNode sortList(ListNode head) {
         if(head == null || head.next == null)
         {
-            return null;
+            return head;
         }
         ListNode fast = head.next, slow = head;
         while (fast != null && fast.next != null)
