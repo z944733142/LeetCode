04a9c42ac87f616296779ccad14e77497d7491eb
z944733142 <944733142@qq.com>
2019-08-23 16:32:01 +0800

    翻转

---
+++ array/RotateArray.java
@@ -0,0 +1,43 @@
+package algorithms.array;
+
+/**
+ * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [1,2,3,4,5,6,7] 和 k = 3
+ * 输出: [5,6,7,1,2,3,4]
+ * 解释:
+ * 向右旋转 1 步: [7,1,2,3,4,5,6]
+ * 向右旋转 2 步: [6,7,1,2,3,4,5]
+ * 向右旋转 3 步: [5,6,7,1,2,3,4]
+ *
+ * @author: shuo
+ * @date: 2019/08/23
+ */
+public class RotateArray {
+    public static void main(String[] args) {
+        RotateArray r = new RotateArray();
+        r.rotate(new int[]{1, 2, 3, 4, 5, 6}, 2);
+    }
+
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+
+    }
+
+    public void reverse(int[] nums, int start, int end)
+    {
+        while (start < end)
+        {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
